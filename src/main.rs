ffd26aeb56ceaa559499fa1381916a7fc772d8b3
amy null <null@amy.gg>
2021-03-14 11:11:26 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,139 @@
+extern crate image;
+extern crate scrap;
+
+use std::io::ErrorKind;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    capture_screenshot();
+}
+
+fn capture_screenshot() {
+    println!("kyoyu: setup: start");
+
+    match scrap::Display::all() {
+        Ok(displays) => {
+            let captures = capture_all_displays(displays);
+            let x = captures
+                .iter()
+                .min_by(|(_, x1, _, _, _), (_, x2, _, _, _)| x1.cmp(x2))
+                .map(|(_, x, _, _, _)| x)
+                .unwrap();
+            let y = captures
+                .iter()
+                .min_by(|(_, _, y1, _, _), (_, _, y2, _, _)| y1.cmp(y2))
+                .map(|(_, _, y, _, _)| y)
+                .unwrap();
+            let w = captures
+                .iter()
+                .max_by(|(_, x1, _, w1, _), (_, x2, _, w2, _)| (x1 + w1).cmp(&(x2 + w2)))
+                .map(|(_, x, _, w, _)| x + w)
+                .unwrap();
+            let h = captures
+                .iter()
+                .max_by(|(_, _, y1, _, h1), (_, _, y2, _, h2)| (y1 + h1).cmp(&(y2 + h2)))
+                .map(|(_, _, y, _, h)| y + h)
+                .unwrap();
+
+            println!("kyoyu: output: dimensions: {}x{} {}x{}", x, y, w, h);
+
+            let mut canvas: image::RgbImage = image::ImageBuffer::new(w as u32, h as u32);
+            // BGR buffers
+            for (buffer, ix, iy, iw, ih) in captures {
+                let stride = buffer.len() / ih;
+                println!(
+                    "kyoyu: output: buffer: processing {}x{} {}x{}",
+                    ix, iy, iw, ih
+                );
+                for nx in 0..iw {
+                    for ny in 0..ih {
+                        let i = stride * ny + 3 * nx;
+                        let pixel = image::Rgb(
+                            [
+                                buffer[i],
+                                buffer[i + 1],
+                                buffer[i + 2],
+                            ]
+                        );
+                        canvas.put_pixel((ix + nx) as u32, (iy + ny) as u32, pixel);
+                    }
+                }
+            }
+            println!("kyoyu: output: saving");
+            canvas.save("screenshot.png").unwrap();
+        }
+        Err(_) => panic!("kyoyu: display: couldn't get all displays"),
+    }
+
+    // let path = Path::new(r"screenshot.png");
+    // let file = File::create(path).unwrap();
+    // let ref mut writer = BufWriter::new(file);
+
+    // let mut encoder = png::Encoder::new(writer, w as u32, h as u32);
+    // encoder.set_color(png::ColorType::RGB);
+    // encoder.set_depth(png::BitDepth::Eight);
+    // let mut writer = encoder.write_header().unwrap();
+
+    // writer.write_image_data(&flipped).unwrap();
+
+    // println!("kyoyu: capture: saved screenshot.png");
+}
+
+/// Capture all provided displays, returning a vec of tuples of image buffers +
+/// bounds
+fn capture_all_displays(
+    displays: Vec<scrap::Display>,
+) -> Vec<(Vec<u8>, usize, usize, usize, usize)> {
+    let mut captures = vec![];
+    for display in displays {
+        let x = display.x();
+        let y = display.y();
+        let w = display.width();
+        let h = display.height();
+        captures.push((capture_display(display), x, y, w, h));
+    }
+    captures
+}
+
+/// Capture the display. Returns a buffer.
+fn capture_display(display: scrap::Display) -> Vec<u8> {
+    let one_second = Duration::new(1, 0);
+    let one_frame = one_second / 60;
+
+    let mut capturer = match scrap::Capturer::new(display) {
+        Ok(capturer) => capturer,
+        Err(_) => panic!("kyoyu: capturer: couldn't create"),
+    };
+    let (w, h) = (capturer.width(), capturer.height());
+    println!("kyoyu: setup: finish");
+
+    // Capture our single frame
+    let buffer = loop {
+        match capturer.frame() {
+            Ok(buffer) => break buffer,
+            Err(err) => {
+                if err.kind() == ErrorKind::WouldBlock {
+                    // Spin until we can get a frame
+                    thread::sleep(one_frame);
+                    continue;
+                }
+            }
+        }
+    };
+
+    println!("kyoyu: frame: captured");
+
+    let mut flipped = Vec::with_capacity(w * h * 3);
+    let stride = buffer.len() / h;
+    for y in 0..h {
+        for x in 0..w {
+            let i = stride * y + 4 * x;
+            flipped.extend_from_slice(&[buffer[i + 2], buffer[i + 1], buffer[i]]);
+        }
+    }
+
+    println!("kyoyu: buffer: flipped");
+
+    flipped
+}
